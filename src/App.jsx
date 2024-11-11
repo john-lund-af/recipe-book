b637609e06cc2974bbcfa04505de20f5f61a5b95
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-11 16:16:01 +0100

    Routing configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Start Page</h1>} />
+      <Route path="/recipe/new" element={<h1>New Recipe</h1>}/>
+      <Route path="/recipe/:id" element={<h1>Tacos Recipe</h1>} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
   )
 }
 
