d9ef5c83fd99e0ed74b344582c0cfc0ee983f607
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-12 18:22:47 +0100

    Added HomePage to route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import RecipePage from './pages/RecipePage'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<h1>Start Page</h1>} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/recipe/new" element={<h1>New Recipe</h1>}/>
       <Route path="/recipe/:id" element={<RecipePage/>} />
       <Route path="*" element={<h1>Not Found</h1>} />
