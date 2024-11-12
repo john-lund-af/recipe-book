d2cae7294a78a4d6f6e1e220ecdac824ca25ea1d
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-12 19:52:36 +0100

    Added Header comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import RecipePage from './pages/RecipePage'
 import HomePage from './pages/HomePage'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/recipe/new" element={<h1>New Recipe</h1>}/>
-      <Route path="/recipe/:id" element={<RecipePage/>} />
-      <Route path="*" element={<h1>Not Found</h1>} />
-    </Routes>
+    <>
+      <Header title="Recipe Book" />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/new" element={<h1>New Recipe</h1>}/>
+        <Route path="/recipe/:id" element={<RecipePage/>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+function Header({title}) {
+  return (
+    <div className="h-24 w-full bg-skin-fill">
+      <h1 className="pt-6 text-5xl text-center text-skin-inverted font-title">{title}</h1>
+    </div>
+  )
+}
+
+Header.propTypes = {
+  title: PropTypes.string
+}
+
+export default Header
