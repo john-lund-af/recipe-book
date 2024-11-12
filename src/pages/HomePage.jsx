1e9e2d70610659e1b457a4f583e65006d05bf880
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-12 18:22:24 +0100

    Added HomePage comp.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import RecipeCard from '../components/RecipeCard';
+import dummyData from '../assets/data/dummyData.json';
+import {useState} from 'react';
+
+function HomePage() {
+  const [recipes] = useState(dummyData.recipes);
+
+  return (
+    <main>
+      <h1>HOME PAGE</h1>
+      <section id="cardList" className="flex flex-row justify-center gap-3 flex-wrap">
+        {recipes.map(recipe => <RecipeCard key={recipe.id} recipe={recipe}  />)}
+      </section>
+    </main>
+  )
+}
+
+export default HomePage
