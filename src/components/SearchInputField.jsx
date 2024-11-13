b99f9b48b680657d99e5fce8c6a193b7411a284a
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-13 10:24:13 +0100

    Search by title functionality.

--- src/components/SearchInputField.jsx
+++ src/components/SearchInputField.jsx
@@ -1,9 +1,18 @@
-function SearchInputField() {
+// import {useState} from 'react';
+import PropTypes from 'prop-types';
+
+function SearchInputField({onSearchValue}) {
+  // const [localSearchValue, setLocalSearchValue] = useState("");
+
   return (
     <div className="flex-1 text-center">
-      <input className="font-input text-skin-secondary w-3/4 p-2 border border-gray-300 rounded-lg text-lg" type="text" name="filtering" id="filtering" placeholder="Search for title..." />
+      <input onChange={(e) => onSearchValue(e.target.value)} className="font-input text-skin-secondary w-3/4 p-2 border border-gray-300 rounded-lg text-lg" type="text" name="filtering" id="filtering" placeholder="Search for title..." />
     </div>
   )
+}
+
+SearchInputField.propTypes = {
+  onSearchValue: PropTypes.func.isRequired
 }
 
 export default SearchInputField
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,14 +5,23 @@ import SearchInputField from '../components/SearchInputField';
 
 function HomePage() {
   const [recipes] = useState(dummyData.recipes);
+  const [searchValue, setSearchValue] = useState("");
+
+  function handleSearchValue(value){
+    setSearchValue(value)
+  }
+
+  const filteredRecipes = !searchValue ? [...recipes] : recipes.filter(recipe => {
+    return recipe.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
+  });
 
   return (
     <main>
       <section className='fixed w-full flex flex-row justify-center bg-gray-100 py-4' id="filter">
-        <SearchInputField />
+        <SearchInputField onSearchValue={handleSearchValue} />
       </section>
       <section id="cardList" className="pt-32 flex flex-row justify-center gap-4 flex-wrap">
-        {recipes.map(recipe => <RecipeCard key={recipe.id} recipe={recipe}  />)}
+        {filteredRecipes.map(recipe => <RecipeCard key={recipe.id} recipe={recipe}  />)}
       </section>
     </main>
   )
