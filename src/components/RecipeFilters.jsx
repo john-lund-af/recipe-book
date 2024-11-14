eea52d747eeba3dc08e8e4394728375ce9418b30
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-14 08:31:29 +0100

    Added useState and useEffect hooks for state changes on filtering.

--- src/components/RecipeFilters.jsx
+++ src/components/RecipeFilters.jsx
@@ -1,19 +1,25 @@
 import PropTypes from 'prop-types';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 const cuisineTypes = ["American","Asian","Brazilian","Greek","Indian","Italian","Japanese","Korean","Lebanese","Mediterranean","Mexican","Moroccan","Pakistani","Russian","Smoothie","Thai","Turkish"];
 
-function RecipeFilters({onSearchValue}) {
-  const [selectedMealType, setSelectedMealType] = useState("all");
+function RecipeFilters({onFilterChange}) {
+  const [selectedCuisine, setSelectedCuisine] = useState("all");
+  const [recipeNameSearch, setRecipeNameSearch] = useState("");
+
+  useEffect(() => {
+    onFilterChange(recipeNameSearch, selectedCuisine);
+    console.log(recipeNameSearch, selectedCuisine);
+  }, [recipeNameSearch, selectedCuisine, onFilterChange]);
 
   return (
     <div className="flex flex-row justify-center font-input">
       <div className='flex-1 text-center' id="searchField">
-        <input onChange={(e) => onSearchValue(e.target.value)} className="w-full lg:w-2/4 p-2 border border-gray-300 rounded-lg text-lg" type="text" name="filtering" id="filtering" placeholder="Search for title..." />
+        <input onChange={(e) => setRecipeNameSearch(e.target.value)} className="w-full lg:w-2/4 p-2 border border-gray-300 rounded-lg text-lg" type="text" name="filtering" id="filtering" placeholder="Search for title..." />
       </div>
       <div className="flex-1 text-center">
-        <select value={selectedMealType} onChange={(e) => setSelectedMealType(e.target.value)} className="h-12 border border-gray-300 text-gray-600 text-base rounded-lg w-full lg:w-2/4 focus:outline-none">
-          <option selected value="all">Choose a meal type</option>
+        <select value={selectedCuisine} onChange={(e) => setSelectedCuisine(e.target.value)} className="h-12 border border-gray-300 text-gray-600 text-base rounded-lg w-full lg:w-2/4 focus:outline-none">
+          <option value="all">Choose a meal type</option>
           {cuisineTypes.map(ct => <option key={ct} value={ct}>{ct}</option>)}
         </select>
       </div>
@@ -22,7 +28,7 @@ function RecipeFilters({onSearchValue}) {
 }
 
 RecipeFilters.propTypes = {
-  onSearchValue: PropTypes.func.isRequired
+  onFilterChange: PropTypes.func.isRequired
 }
 
 export default RecipeFilters
