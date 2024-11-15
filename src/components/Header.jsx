6b8fa782444c1a33e6d1207df66ee089a1a9042b
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-15 10:41:46 +0100

    Added links to Home and About page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBowlFood } from '@fortawesome/free-solid-svg-icons'
+import {NavLink} from 'react-router-dom';
 
 function Header({title}) {
   return (
-    <header className="fixed h-24 w-full bg-skin-fill shadow-2xl">
-      <h1 className="pt-6 text-5xl text-center text-skin-inverted font-title">{title}</h1>
+    <header className="px-2 md:px-4 lg:px-6 text-skin-inverted fixed h-24 w-full bg-skin-fill shadow-2xl flex flex-row justify-between items-center">
+      <div className="flex flex-row items-center gap-2">
+        <span>
+          <FontAwesomeIcon icon={faBowlFood} size="2x" />
+        </span>
+        <span className="text-lg text-center font-title">{title}</span>
+      </div>
+      <nav>
+        <ul className="flex flex-row gap-2 text-lg">
+          <li><NavLink to="/" className={({isActive}) => isActive ? 'text-red-700 underline' : ''}>Home</NavLink></li>
+          <li><NavLink to="/about" className={({isActive}) => isActive ? 'text-red-700 underline' : ''}>About</NavLink></li>
+        </ul>
+      </nav>
     </header>
   )
 }
